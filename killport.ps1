65e3a2597d8d7b38ad6ada749c00f6d9be0765d3
Siamak Kosari <48735465+skosari@users.noreply.github.com>
2026-04-18 17:29:22 -0700

    Bump version to 1.10.14

--- killport.ps1
+++ killport.ps1
@@ -4,7 +4,7 @@ param(
     [Parameter(Mandatory=$false, Position=2)] [string]$Extra
 )
 
-$VERSION = "1.10.13"
+$VERSION = "1.10.14"
 $REPO    = "skosari/killport-win"
 $RAW     = "https://raw.githubusercontent.com/$REPO/main"
 
